8638c16c8842eec2b356d3bad50f557e76d87470
KoshJR <goldenarm31.03@gmail.com>
2023-10-22 15:09:20 -0400

    fin

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,33 @@
 // Add imports above this line
+import 'simplelightbox/dist/simple-lightbox.min.css';
 import { galleryItems } from './gallery-items';
+// Описаний в документації
+import SimpleLightbox from 'simplelightbox';
+// Додатковий імпорт стилів
+
+const imageBox = document.querySelector('.gallery');
+imageBox.style.listStyle = 'none';
+const markup = galleryItems
+  .map(
+    item =>
+      `<li class="gallery__item">
+  <a class="gallery__link" href="${item.original}">
+    <img
+      class="gallery__image"
+      src="${item.preview}"
+      alt="${item.description}"
+    />
+  </a>
+</li>`
+  )
+  .join('');
+imageBox.innerHTML = markup;
+
+let lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+
 // Change code below this line
 
 console.log(galleryItems);
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,34 @@
+import throttle from 'lodash.throttle';
+const formInput = document.querySelector('.feedback-form');
 
+let clientData = {};
+const textInputTrottle = throttle(textInput, 500);
+formInput.addEventListener('input', textInputTrottle);
+
+function textInput(event) {
+  clientData[event.target.name] = event.target.value;
+  localStorage.setItem('feedback-form-state', JSON.stringify(clientData));
+}
+
+function localStorageCheck() {
+  const userdata = JSON.parse(localStorage.getItem('feedback-form-state'));
+  if (userdata !== null) {
+    clientData = userdata;
+    for (const key in userdata) {
+      formInput.elements[key].value = userdata[key];
+    }
+  }
+}
+localStorageCheck();
+
+formInput.addEventListener('submit', clickOnSubmit);
+function clickOnSubmit(evt) {
+  evt.preventDefault();
+  if (evt.target.email.value === '' || evt.target.message.value === '') {
+    return alert('Please fill in all the fields!');
+  }
+  console.log(clientData);
+  evt.target.reset();
+  localStorage.removeItem('feedback-form-state');
+  clientData = {};
+}
